9966dd94117b5aec731311bd345767ae75045e94
Colin Crampton <ccrampton@mindgrub.com>
2019-08-03 17:13:46 -0400

    Deploy fix.

--- src/server/room.js
+++ src/server/room.js
@@ -2,7 +2,20 @@ let Sentencer = require('sentencer');
 
 module.exports = {
     genRoomId : ()  => {
-        return Sentencer.make("{{adjective}} COCK").toUpperCase();
+        let big = [
+            "substantial", "considerable", "great", "huge", "immense", "enormous", "extensive", "colossal", "massive", "mammoth", "vast", "prodigious", "tremendous", "gigantic", "giant", "monumental", "mighty", "stupendous", "gargantuan", "elephantine", "titanic", "epic", "mountainous", "megalithic", "monstrous"
+        ];
+
+        let currency = [
+            "rupee", "dong", "dollar", "pound", "shilling", "peso", "ruble", "won", "yen", "crone"
+        ];
+
+        let first = big[Math.floor(Math.random()*big.length)];
+        let second = currency[Math.floor(Math.random()*currency.length)];
+
+        let roomCode = first + " " + second;
+
+        return roomCode.toUpperCase();
     },
     addPlayer : (room, player) => {
         if (room && player) {
